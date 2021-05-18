c8735637a08d99ecdcbad69a00bb6b1a283b3685
sidansari20@ <sidansari20@gmail.com>
2021-05-19 02:58:12 +0530

    power_of_a_no

---
+++ my_codes/coading_ninja/basic_test/find_power_of_a_number.py
@@ -0,0 +1,13 @@
+"""
+Write a program to find x to the power n (i.e. x^n). Take x and n from the user. You need to print the answer.
+Note : For this question, you can assume that 0 raised to the power of 0 is 1
+
+
+Input format :
+Two integers x and n (separated by space)
+
+"""
+## Read input as specified in the question.
+## Print output as specified in the question.
+x,n = input().split(' ')
+print(int(x)**int(n))
