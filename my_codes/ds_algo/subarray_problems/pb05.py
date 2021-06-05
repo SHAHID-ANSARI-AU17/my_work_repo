9a454067ca532690f1f7cccaa6387a59b49d7be7
sidansari20@ <sidansari20@gmail.com>
2021-06-05 20:25:30 +0530

    sub_array_problem

---
+++ my_codes/ds_algo/subarray_problems/pb05.py
@@ -0,0 +1,38 @@
+"""
+sample input
+3
+1 2 3
+sample output
+
+# Print count of sub array in which every element is even
+n = 3
+array = [1,2,3]
+Sub array are :
+[1] 
+[1,2]
+[1,2,3]
+[2]
+[2,3]
+[3]
+
+sub array that have only even no. are :-> sum are : [2]
+so the answer is - >  1
+
+"""
+
+n = 3
+a = [1,2,3]
+count_main = 0
+for i in range(n):
+    res = []
+    for j in range(i,n):
+        res.append(a[j])
+        count_even = 0
+        for q in res:
+            if q%2==0:
+                count_even += 1
+        if count_even == len(res):
+            count_main += 1
+print(count_main)
+
+
